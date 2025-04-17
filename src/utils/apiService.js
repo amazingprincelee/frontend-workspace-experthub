1bb97ae8994d76257c2b7a03066c718894849599
Prince Lee <prince_lee.nna85@ymail.com>
2025-04-17 17:48:15 +0100

    update client dashboard

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 
 const apiService = axios.create({
-  // baseURL: "https://backend-workspace-experthub.onrender.com",
-  baseURL: "http://localhost:3002/",
+   baseURL: "https://backend-workspace-experthub.onrender.com",
+  // baseURL: "http://localhost:3002/",
   withCredentials: false,
 });
 
